e1761692bcd647b77664b312f11d41cb3ef17b75
CaynanMacedo <caynanmacedo2@gmail.com>
2026-08-14 17:09:35 -0300

    calculadora

--- 02_atribuicoes.js
+++ 02_atribuicoes.js
@@ -1,9 +1,8 @@
 //AULA 2 ATRIBUIÇÕES DE VALORES 
-let preco = 19.90 // float = casas decimais
-let cor = "Azul" // string = texto
+let preco = 19.90 
+let cor = "Azul" 
 let nome = "Tenis" 
-let tamanho = 40 // int = numero inteiro 
-let frete_gratis = true //boolean ou bool = true or false 0 1 
-//variavel não pode ter caracter especial, acento, numero no começo nem maiusculo
+let tamanho = 40  
+let frete_gratis = true 
 
 console.log("\n venda realizada no valor de R$" +preco+ " item [" +nome+ "] tamanho " +tamanho+ " na cor " +cor+ " Frete grátis: " +frete_gratis)
---
+++ 02_operacoes.js
@@ -0,0 +1,17 @@
+//AULA 2 = operações matematicas com variaveis
+//Calculadorinha
+
+let numero1 = 5
+let numero2 = 5
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
+let media = (numero1 + numero2) / 2
+
+console.log("A soma de " +numero1+ " é " +numero2+ " é igual a " +soma)
+console.log("A subtração de " +numero1+ " é " +numero2+ " é igual a " +subtracao)
+console.log("A multiplicação de " +numero1+ " é " +numero2+ " é igual a " +multiplicacao)
+console.log("A divisão de " +numero1+ " é " +numero2+ " é igual a " +divisao)
+console.log("A media de " +numero1+ " é " +numero2+ " é igual a " +media)
