dff7c402c18e68fb4495255a6151e8f7168ac323
CaynanMacedo <caynanmacedo2@gmail.com>
2026-08-14 16:30:53 -0300

    atividade

---
+++ 02_atividade_variaveis.js
@@ -0,0 +1,15 @@
+//atividade do conradito
+
+let nome = "Caynan Macedo de Sousa"
+let idade = 19
+let cpf = 21212121212
+let endereco = "Rua Pedro Gomes Escobar 43"
+let telefone = "(16)99341-6265"
+let nascimento = "04/12/2006"
+let saldo_inicial = 550.99
+let cartao_credito = false
+let cartao_debito = true
+
+console.log("Caro " +nome+ " nascido em " +nascimento+ " tendo " +idade+ " anos de idade e cpf de numero " +cpf);
+console.log("residente na " +endereco+ " com telefone numero " +telefone);
+console.log("o estado atual do seu cartão de credito é " +cartao_credito+ " e do cartão de debito é " +cartao_debito+ " tendo o saldo inicial de " +saldo_inicial);
--- 02_atribuicoes.js
+++ 02_atribuicoes.js
@@ -1,2 +1,9 @@
 //AULA 2 ATRIBUIÇÕES DE VALORES 
-let preco = 19.90
+let preco = 19.90 // float = casas decimais
+let cor = "Azul" // string = texto
+let nome = "Tenis" 
+let tamanho = 40 // int = numero inteiro 
+let frete_gratis = true //boolean ou bool = true or false 0 1 
+//variavel não pode ter caracter especial, acento, numero no começo nem maiusculo
+
+console.log("\n venda realizada no valor de R$" +preco+ " item [" +nome+ "] tamanho " +tamanho+ " na cor " +cor+ " Frete grátis: " +frete_gratis)
